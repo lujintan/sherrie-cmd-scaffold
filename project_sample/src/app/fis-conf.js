87a577505054cf598900af3375d7b80433bc6261
yingxuetan <yingxuetan@163.com>
2015-04-09 14:33:42 +0800

    update sample

--- project_sample/src/app/fis-conf.js
+++ project_sample/src/app/fis-conf.js
@@ -30,22 +30,24 @@ fis.config.merge({
     <%} else {%>
         //文件打包配置
         pack: {
-            '/static/app.js': [
+            '/static/app/app.js': [
                 /\/static\/js\/core\/\w*.js/,
                 /\/static\/js\/app\/\w*.js/,
                 /\/widget\/\w+\/[\w\W]*.js/
             ],
-            '/static/app.css': /\/widget\/\w+\/\w*.(?:scss|css)/
+            '/static/app/app.css': /\/widget\/\w+\/\w*.(?:scss|css)/
         }
     <%}%>
 });
 
 fis.config.get('roadmap.path').unshift({
-    reg: /\/static\/js\/app\/(?!.*[init]\.js$)[\s\S]+/,
+    reg: /\/static\/js\/app\/((?!.*[init]\.js$)[\s\S]+)/,
+    release: '/static/${namespace}/js/app/$1',
     isMod: true
 });
 
 fis.config.get('roadmap.path').unshift({
-    reg: /\/static\/js\/core\/(?!.*[invoke|ofa]\.js$)[\s\S]+/,
+    reg: /\/static\/js\/core\/((?!.*[invoke|ofa]\.js$)[\s\S]+)/,
+    release: '/static/${namespace}/js/core/$1',
     isMod: true
 });
